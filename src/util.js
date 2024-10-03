78e8efff73dfe0fc8bdbf60e2ec7d65f25fbef5e
J-Dog <j-dog@j-dog.net>
2024-10-02 21:22:18 -0700

    add mathjs support

--- src/util.js
+++ src/util.js
@@ -1,6 +1,7 @@
 /* XChain Indexer Utility Functions */
 
 const crypto = require('crypto');
+const mathjs = require('mathjs');
 
 module.exports = {
 
@@ -59,6 +60,82 @@ module.exports = {
         if(minutes > 0) str += minutes + 'm ';
         if(seconds > 0) str += seconds + '.' + milliseconds + 's';
         return str;
-    }    
+    },
+
+    // Determine if a value is numeric
+    isNumeric: function(value){
+        return !isNaN(parseFloat(value)) && isFinite(value);
+    },
+
+    // Determine if value is floating point
+    isFloat: function(value){
+        return value === +value && value !== (value|0);
+    },
+
+    // Determine if value is integer
+    isInteger: function(value){
+        return value === +value && value === (value|0);
+    },
+
+    // Determine if value is null or undefined
+    isNull: function(value){
+        return (value === null || value === undefined);
+    },
+
+    // Handle determining if first param is TICK or VERSION
+    isLegacyActionFormat: function(params){
+        version = params[0]; // VERSION or TICK
+        // VERSION will max out at 99 (2 chars)
+        if(String(version).length>2)
+            return true;
+        // VERSION should be NULL or integer
+        if(typeof version === 'string' && !this.isNumeric(version))
+            return true;
+        // Add more rules here if ppl keep using old format
+        return false;
+    },
+
+    // Handle returning integer format version
+    getFormatVersion: function(format){
+        let type = typeof format;
+        if(type=='number' && this.isInteger(format))
+            return format;
+        // Default to format 0 if none is given
+        if(type=='undefined' || (type=='string' && format==''))
+            return 0;
+        // Strip out any quotes and double-quotes
+        if(type=='string')
+            format = format.replace(/\"|\'/g,'');
+        // Convert any numeric strings to integers
+        if(this.isNumeric(format) && !this.isFloat(format))
+            return parseInt(format);
+        // Return NULL if not able to identify format version
+        return null;
+    },
+
+    // Handle setting ACTION PARAMS based on format VERSION (updates ACTION transaction data object)
+    setActionParams: function(data, params, format){
+        let fields = String(format).split('|');
+        for(idx in fields){
+            let field = fields[idx];
+            let value = null;
+            if(typeof params[idx] !== 'undefined')
+                value = String(params[idx]).trim();
+            data[field] = value;
+        }
+        return data;
+    },
+
+    // Handle subtracting 2 big numbers
+    bcsub: function(a, b, decimals){
+        let precision = (decimals) ? decimals : 0;
+        return result = mathjs.format(mathjs.subtract(mathjs.bignumber(a),mathjs.bignumber(b)),{notation: 'fixed', precision: precision});
+    },
+
+    // Handle adding 2 big numbers
+    bcadd: function(a, b, decimals){
+        let precision = (decimals) ? decimals : 0;
+        return result = mathjs.format(mathjs.add(mathjs.bignumber(a),mathjs.bignumber(b)),{notation: 'fixed', precision: precision});
+    }
 
 }
