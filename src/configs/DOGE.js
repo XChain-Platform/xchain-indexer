eb6c9bf8c6936c90427b31f5e8c180040b6faa1c
J-Dog <j-dog@j-dog.net>
2025-04-15 09:37:30 -0700

    updates to support indexer and coin-specific configurations

---
+++ src/configs/DOGE.js
@@ -0,0 +1,41 @@
+/*********************************************************************
+ * XChain COIN Configuration - Dogecoin (DOGE) 
+ * 
+ * This config file contains COIN specific configuration information
+ * 
+ ********************************************************************/
+module.exports = {
+
+    // Handle returning the current coin configuration
+    getConfig: function(network){
+
+        // Define config objects
+        let config  = {};
+		let address = {};
+
+		// Set network specific addresses
+        switch(network){
+            case 'mainnet':
+                address['BURN']    = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+                address['GAS']     = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+                address['DONATE1'] = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"; // Protocol Development
+                address['DONATE2'] = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"; // Community Develoment
+                break;
+            case 'testnet':
+                address['BURN']    = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+                address['GAS']     = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+                address['DONATE1'] = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"; // Protocol Development
+                address['DONATE2'] = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"; // Community Develoment
+                break;
+            case 'regtest':
+                address['BURN']    = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+                address['GAS']     = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+                address['DONATE1'] = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"; // Protocol Development
+                address['DONATE2'] = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"; // Community Develoment
+                break;
+        }
+        config['ADDRESS'] = address;
+
+        return config;
+    }
+}
