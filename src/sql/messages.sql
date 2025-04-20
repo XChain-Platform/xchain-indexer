40466510c20d5a5a404a331ff68559d81e0a1d08
J-Dog <j-dog@j-dog.net>
2025-04-19 20:32:57 -0700

    add messages table

---
+++ src/sql/messages.sql
@@ -0,0 +1,17 @@
+DROP TABLE IF EXISTS messages;
+CREATE TABLE messages (
+    action_index        BIGINT UNSIGNED NOT NULL, -- Unique action index
+    destination_id      BIGINT UNSIGNED,          -- id of record in index_addresses table
+    source_id           BIGINT UNSIGNED,          -- id of record in index_addresses table
+    encryption_method   VARCHAR(1),               -- Encryption Method (1=ECDH, 2=AES)
+    encryption_key      MEDIUMTEXT,               -- Public key to be used to exchange messages
+    encrypted_message   MEDIUMTEXT,               -- Encrypted Message
+    plaintext_message   MEDIUMTEXT,               -- Plaintext Message
+    status_id           BIGINT UNSIGNED           -- id of record in index_statuses table
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+CREATE UNIQUE INDEX action_index      ON messages (action_index);
+CREATE        INDEX encryption_method ON messages (encryption_method);
+CREATE        INDEX source_id         ON messages (source_id);
+CREATE        INDEX destination_id    ON messages (destination_id);
+CREATE        INDEX status_id         ON messages (status_id);
