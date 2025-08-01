9ea608380dcd0379e1059dee9286bed1f2832066
J-Dog <j-dog@j-dog.net>
2025-08-01 12:05:37 -0700

    add source_id to dispenses table as purchasing address

--- src/sql/dispenses.sql
+++ src/sql/dispenses.sql
@@ -4,11 +4,11 @@ CREATE TABLE dispenses (
     give_coin_id    BIGINT UNSIGNED,          -- id of record in index_coins table
     give_tick_id    BIGINT UNSIGNED,          -- id of record in index_tickers table
     give_amount     VARCHAR(250),             -- Amount of GIVE_TICK to dispense when triggered
-    give_address_id BIGINT UNSIGNED,          -- id of record in index_addresses table (purchasing address)
     get_coin_id     BIGINT UNSIGNED,          -- id of record in index_coins table
     get_tick_id     BIGINT UNSIGNED,          -- id of record in index_tickers table
     get_amount      VARCHAR(250),             -- Amount of GET_TICK required to trigger dispenser
     get_address_id  BIGINT UNSIGNED,          -- id of record in index_addresses table (dispenser address)
+    source_id       BIGINT UNSIGNED,          -- id of record in index_addresses table (purchasing address)
     status_id       BIGINT UNSIGNED           -- id of record in index_statuses table
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
@@ -16,8 +16,8 @@ CREATE TABLE dispenses (
 CREATE UNIQUE INDEX action_index    ON dispenses (action_index);
 CREATE        INDEX give_coin_id    ON dispenses (give_coin_id);
 CREATE        INDEX give_tick_id    ON dispenses (give_tick_id);
-CREATE        INDEX give_address_id ON dispenses (give_address_id);
 CREATE        INDEX get_coin_id     ON dispenses (get_coin_id);
 CREATE        INDEX get_tick_id     ON dispenses (get_tick_id);
 CREATE        INDEX get_address_id  ON dispenses (get_address_id);
+CREATE        INDEX source_id       ON dispenses (source_id);
 CREATE        INDEX status_id       ON dispenses (status_id);
