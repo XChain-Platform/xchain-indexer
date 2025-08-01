40e151402a5e1be3238bc2b1d65d38b7cccbdd6f
J-Dog <j-dog@j-dog.net>
2025-08-01 12:00:56 -0700

    cleanup sql table definitions for dispensers and dispenses tables

--- src/sql/dispenses.sql
+++ src/sql/dispenses.sql
@@ -1,19 +1,23 @@
 DROP TABLE IF EXISTS dispenses;
 CREATE TABLE dispenses (
     action_index    BIGINT UNSIGNED NOT NULL, -- Unique action index
-    dispense_index  BIGINT UNSIGNED, 
-    source_id       BIGINT UNSIGNED,          -- id of record in index_addresses (dispenser address)
-    destination_id  BIGINT UNSIGNED,          -- id of record in index_addresses (purchasing address)
-    tick_id         BIGINT UNSIGNED,          -- id of record in index_tickers table
-    amount          VARCHAR(250),              -- Tokens to vend per dispense
-    trigger_tick_id BIGINT UNSIGNED,          -- id of record in index_tickers table
-    trigger_amount  VARCHAR(250),              -- Amount of trigger_tick_id paid in this dispense
+    give_coin_id    BIGINT UNSIGNED,          -- id of record in index_coins table
+    give_tick_id    BIGINT UNSIGNED,          -- id of record in index_tickers table
+    give_amount     VARCHAR(250),             -- Amount of GIVE_TICK to dispense when triggered
+    give_address_id BIGINT UNSIGNED,          -- id of record in index_addresses table (purchasing address)
+    get_coin_id     BIGINT UNSIGNED,          -- id of record in index_coins table
+    get_tick_id     BIGINT UNSIGNED,          -- id of record in index_tickers table
+    get_amount      VARCHAR(250),             -- Amount of GET_TICK required to trigger dispenser
+    get_address_id  BIGINT UNSIGNED,          -- id of record in index_addresses table (dispenser address)
     status_id       BIGINT UNSIGNED           -- id of record in index_statuses table
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
-CREATE UNIQUE INDEX action_index     ON dispenses (action_index);
-CREATE        INDEX source_id        ON dispenses (source_id);
-CREATE        INDEX destination_id   ON dispenses (destination_id);
-CREATE        INDEX tick_id          ON dispenses (tick_id);
-CREATE        INDEX trigger_tick_id  ON dispenses (trigger_tick_id);
-CREATE        INDEX status_id        ON dispenses (status_id);
+
+CREATE UNIQUE INDEX action_index    ON dispenses (action_index);
+CREATE        INDEX give_coin_id    ON dispenses (give_coin_id);
+CREATE        INDEX give_tick_id    ON dispenses (give_tick_id);
+CREATE        INDEX give_address_id ON dispenses (give_address_id);
+CREATE        INDEX get_coin_id     ON dispenses (get_coin_id);
+CREATE        INDEX get_tick_id     ON dispenses (get_tick_id);
+CREATE        INDEX get_address_id  ON dispenses (get_address_id);
+CREATE        INDEX status_id       ON dispenses (status_id);
