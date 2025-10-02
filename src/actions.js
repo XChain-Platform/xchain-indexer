d8bb956330185efc05e8a764eb28e331b7850a67
J-Dog <j-dog@j-dog.net>
2025-10-02 11:26:11 -0700

    added support for `ORDER_MATCH` action

--- src/actions.js
+++ src/actions.js
@@ -1,27 +1,28 @@
 /* XChain Indexer Actions Class */
 
 /* XChain Indexer Actions */
-const address    = require('./actions/address.js');
-const airdrop    = require('./actions/airdrop.js');
-const batch      = require('./actions/batch.js');
-// const bet       = require('./actions/bet.js');
-const broadcast  = require('./actions/broadcast.js');
-const callback   = require('./actions/callback.js');
-const destroy    = require('./actions/destroy.js');
-// const dispenser = require('./actions/dispenser.js');
-const dividend   = require('./actions/dividend.js');
-const file       = require('./actions/file.js');
-const issue      = require('./actions/issue.js');
-const link       = require('./actions/link.js');
-const list       = require('./actions/list.js');
-const message    = require('./actions/message.js');
-const mint       = require('./actions/mint.js');
-const order      = require('./actions/order.js');
-const sleep      = require('./actions/sleep.js');
-const send       = require('./actions/send.js');
-const swap       = require('./actions/swap.js');
-const swap_match = require('./actions/swap_match.js');
-const sweep      = require('./actions/sweep.js');
+const address     = require('./actions/address.js');
+const airdrop     = require('./actions/airdrop.js');
+const batch       = require('./actions/batch.js');
+// const bet         = require('./actions/bet.js');
+const broadcast   = require('./actions/broadcast.js');
+const callback    = require('./actions/callback.js');
+const destroy     = require('./actions/destroy.js');
+// const dispenser   = require('./actions/dispenser.js');
+const dividend    = require('./actions/dividend.js');
+const file        = require('./actions/file.js');
+const issue       = require('./actions/issue.js');
+const link        = require('./actions/link.js');
+const list        = require('./actions/list.js');
+const message     = require('./actions/message.js');
+const mint        = require('./actions/mint.js');
+const order       = require('./actions/order.js');
+const order_match = require('./actions/order_match.js');
+const sleep       = require('./actions/sleep.js');
+const send        = require('./actions/send.js');
+const swap        = require('./actions/swap.js');
+const swap_match  = require('./actions/swap_match.js');
+const sweep       = require('./actions/sweep.js');
 
 class Actions {
 
@@ -60,6 +61,7 @@ class Actions {
         this.actionMessage    = new message(this);
         this.actionMint       = new mint(this);
         this.actionOrder      = new order(this);
+        this.actionOrderMatch = new order_match(this);
         this.actionSleep      = new sleep(this);
         this.actionSend       = new send(this);
         this.actionSwap       = new swap(this);
@@ -159,27 +161,28 @@ class Actions {
         this.util.resetLists();
 
         // Process the action with the correct handler
-        if(action=='ADDRESS')    await this.actionAddress.parse(params, data, error);
-        if(action=='AIRDROP')    await this.actionAirdrop.parse(params, data, error);
-        if(action=='BATCH')      await this.actionBatch.parse(params, data, error);
-        // if(action=='BET')        await this.actionBet.parse(params, data, error);
-        if(action=='BROADCAST')  await this.actionBroadcast.parse(params, data, error);
-        if(action=='CALLBACK')   await this.actionCallback.parse(params, data, error);
-        if(action=='DESTROY')    await this.actionDestroy.parse(params, data, error);
-        // if(action=='DISPENSER')  await this.actionDispenser.parse(params, data, error);
-        if(action=='DIVIDEND')   await this.actionDividend.parse(params, data, error);
-        if(action=='FILE')       await this.actionFile.parse(params, data, error);
-        if(action=='ISSUE')      await this.actionIssue.parse(params, data, error);
-        if(action=='LIST')       await this.actionList.parse(params, data, error);
-        if(action=='LINK')       await this.actionLink.parse(params, data, error);
-        if(action=='MINT')       await this.actionMint.parse(params, data, error);
-        if(action=='MESSAGE')    await this.actionMessage.parse(params, data, error);
-        if(action=='ORDER')      await this.actionOrder.parse(params, data, error);
-        if(action=='SLEEP')      await this.actionSleep.parse(params, data, error);
-        if(action=='SEND')       await this.actionSend.parse(params, data, error);
-        if(action=='SWAP')       await this.actionSwap.parse(params, data, error);
-        if(action=='SWAP_MATCH') await this.actionSwapMatch.parse(params, data, error);
-        if(action=='SWEEP')      await this.actionSweep.parse(params, data, error);
+        if(action=='ADDRESS')     await this.actionAddress.parse(params, data, error);
+        if(action=='AIRDROP')     await this.actionAirdrop.parse(params, data, error);
+        if(action=='BATCH')       await this.actionBatch.parse(params, data, error);
+        // if(action=='BET')         await this.actionBet.parse(params, data, error);
+        if(action=='BROADCAST')   await this.actionBroadcast.parse(params, data, error);
+        if(action=='CALLBACK')    await this.actionCallback.parse(params, data, error);
+        if(action=='DESTROY')     await this.actionDestroy.parse(params, data, error);
+        // if(action=='DISPENSER')   await this.actionDispenser.parse(params, data, error);
+        if(action=='DIVIDEND')    await this.actionDividend.parse(params, data, error);
+        if(action=='FILE')        await this.actionFile.parse(params, data, error);
+        if(action=='ISSUE')       await this.actionIssue.parse(params, data, error);
+        if(action=='LIST')        await this.actionList.parse(params, data, error);
+        if(action=='LINK')        await this.actionLink.parse(params, data, error);
+        if(action=='MINT')        await this.actionMint.parse(params, data, error);
+        if(action=='MESSAGE')     await this.actionMessage.parse(params, data, error);
+        if(action=='ORDER')       await this.actionOrder.parse(params, data, error);
+        if(action=='ORDER_MATCH') await this.actionOrderMatch.parse(params, data, error);
+        if(action=='SLEEP')       await this.actionSleep.parse(params, data, error);
+        if(action=='SEND')        await this.actionSend.parse(params, data, error);
+        if(action=='SWAP')        await this.actionSwap.parse(params, data, error);
+        if(action=='SWAP_MATCH')  await this.actionSwapMatch.parse(params, data, error);
+        if(action=='SWEEP')       await this.actionSweep.parse(params, data, error);
     }
 }
 
---
+++ src/actions/order_match.js
@@ -0,0 +1,140 @@
+/*********************************************************************
+ * XChain Platform Action - ORDER_MATCH
+ * 
+ * This action finds and processes matching order actions
+ *
+ ********************************************************************/
+
+class Order_Match {
+
+    // Handle constructing a class instance
+    constructor(action){
+        // Setup short aliases
+        this.actions   = action;
+        this.config    = action.config;
+        this.decoderDb = action.decoderDb;
+        this.indexerDb = action.indexerDb;
+        this.util      = action.util;
+        this.mapper    = action.mapper;
+    }
+
+    // Handle looking for matching orders
+    async parse(params, data, error){
+
+        // Get information on a order given the COIN network and ORDER_ACTION_INDEX
+        let orderIndex = (data['ORDER_ACTION_INDEX']) ? data['ORDER_ACTION_INDEX'] : data['ACTION_INDEX'];
+        let orderInfo  = await this.indexerDb.getOrderInfo(this.config['COIN'], orderIndex)
+
+        // Get a list of any matching open orders
+        let matches = await this.indexerDb.getOrderMatches(data);
+        // console.log('matches=',matches);
+        if(matches){
+            // Get information on the tokens involved in the order
+            let getTokenInfo  = await this.indexerDb.getTokenInfo(data['GET_TICK'],  data['BLOCK_INDEX'], data['ACTION_INDEX']);
+            let giveTokenInfo = await this.indexerDb.getTokenInfo(data['GIVE_TICK'], data['BLOCK_INDEX'], data['ACTION_INDEX']);
+
+            // List of addresses allowed or blocked from holding GET_TICK
+            let getTokenAllowList = (getTokenInfo['ALLOW_LIST']) ? await this.indexerDb.getList(getTokenInfo['ALLOW_LIST']) : false;
+            let getTokenBlockList = [getTokenInfo['BLOCK_LIST']] ? await this.indexerDb.getList(getTokenInfo['BLOCK_LIST']) : false;
+
+            // List of addresses allowed or blocked from holding GIVE_TICK
+            let giveTokenAllowList = (giveTokenInfo['ALLOW_LIST']) ? await this.indexerDb.getList(giveTokenInfo['ALLOW_LIST']) : false;
+            let giveTokenBlockList = [giveTokenInfo['BLOCK_LIST']] ? await this.indexerDb.getList(giveTokenInfo['BLOCK_LIST']) : false;
+
+            // List of addresses allowed or blocked from matching with this ORDER
+            let orderInfoAllowList = (orderInfo['ALLOW_LIST']) ? await this.indexerDb.getList(orderInfo['ALLOW_LIST']) : false;
+            let orderInfoBlockList = [orderInfo['BLOCK_LIST']] ? await this.indexerDb.getList(orderInfo['BLOCK_LIST']) : false;
+
+            // Loop through matches and determine if we have a valid match
+            let match = false;
+            for(let order of matches){
+                let valid = true;
+
+                   // List of addresses allowed or blocked from matching with this matching SWAP
+                   let matchInfoAllowList = (order['ALLOW_LIST']) ? await this.indexerDb.getList(order['ALLOW_LIST']) : false;
+                   let matchInfoBlockList = [order['BLOCK_LIST']] ? await this.indexerDb.getList(order['BLOCK_LIST']) : false;
+
+                // Check if GET_ADDRESS for both sides of order are allowed (ALLOW/BLOCK list support)
+                if( (getTokenAllowList.length  && (!getTokenAllowList.includes(data['GET_ADDRESS'])   || !getTokenAllowList.includes(order['GET_ADDRESS'])))  ||
+                    (getTokenBlockList.length  && ( getTokenBlockList.includes(data['GET_ADDRESS'])   ||  getTokenBlockList.includes(order['GET_ADDRESS'])))  ||
+                    (giveTokenAllowList.length && (!giveTokenAllowList.includes(data['GET_ADDRESS'])  || !giveTokenAllowList.includes(order['GET_ADDRESS']))) ||
+                    (giveTokenBlockList.length && ( giveTokenBlockList.includes(data['GET_ADDRESS'])  ||  giveTokenBlockList.includes(order['GET_ADDRESS'])))  ||
+                    (orderInfoAllowList.length && !orderInfoAllowList.includes(order['GET_ADDRESS'])) ||
+                    (orderInfoBlockList.length &&  orderInfoBlockList.includes(order['GET_ADDRESS'])) || 
+                    (matchInfoAllowList.length && !matchInfoAllowList.includes(data['GET_ADDRESS']))  ||
+                    (matchInfoBlockList.length &&  matchInfoBlockList.includes(data['GET_ADDRESS']))){
+                    valid = false;
+                }
+
+                // If we found a valid match, process the order match and update the escrowed amounts
+                // TODO : Revisit this code once multi-chain order support is added to xchain-hub component
+                if(valid){
+
+                    // TODO: Add support for partial matches... currently only support exact matches
+
+                    // Set the status to valid
+                    data['STATUS'] = 'valid';
+
+                    // Print status message
+                    console.log("\t ORDER_MATCH : " + data['GIVE_AMOUNT'] + ' ' + this.config['COIN'] + ':' + data['GIVE_TICK'] + ' = '  +  data['GET_AMOUNT'] + ' ' + data['GET_COIN'] + ':' + data['GET_TICK'] + ' : ' + data['STATUS']);
+
+                    // Pass forward information on this order match
+                    match = order;
+
+                    // Array of credits, debits, and escrows
+                    let credits = [],
+                        debits  = [],
+                        escrows = [];
+
+
+                    // Define ORDER_MATCH action
+                    let action = {}
+                    action['BLOCK_INDEX'] = data['BLOCK_INDEX'];
+                    action['TX_INDEX']    = data['TX_INDEX']
+                    action['ACTION']      = 'ORDER_MATCH';
+
+                    // Create a record of this ORDER_MATCH action in the actions table
+                    data['ACTION_INDEX'] = await this.indexerDb.createActionIndex(action);
+
+                    // Store the SOURCE and GET_TICK in addresses list
+                    this.util.addAddressTicker(match['SOURCE'], match['GET_TICK']);
+
+                    // Credit tokens to GET_ADDRESS in orders
+                    credits.push([match['GET_TICK'], match['GET_AMOUNT'], match['GET_ADDRESS']]);
+                    credits.push([data['GET_TICK'],  data['GET_AMOUNT'],  data['GET_ADDRESS']]);
+
+                    // Debit tokens from escrows table 
+                    escrows.push([match['GET_TICK'], -match['GET_AMOUNT'], data['SOURCE']]);
+                    escrows.push([data['GET_TICK'],  -data['GET_AMOUNT'],  match['SOURCE']]);
+
+                    // Store the GET_ADDRESS and TICK in addresses list
+                    this.util.addAddressTicker(match['GET_ADDRESS'], match['GET_TICK']);
+                    this.util.addAddressTicker(data['GET_ADDRESS'],  data['GET_TICK']);
+
+                    // Process any transaction ledger changes (credits / debits / escrows)
+                    await this.util.processTransactionLedgerChanges(this.indexerDb, data, credits, debits, escrows);
+
+                    // Create record of match in order_matches table
+                    await this.indexerDb.createOrderMatch(data['ACTION_INDEX'], order, match, 'valid');
+
+                    // TODO verify if order still has some GET_TICK and GIVE_TICK quantity. if not, change status to filled
+                    // Update record in orders table to change status (open->filled)
+                    await this.indexerDb.createOrderStatus(data['ACTION_INDEX'], order['ACTION_INDEX'], 'filled');
+                    await this.indexerDb.createOrderStatus(data['ACTION_INDEX'], match['ACTION_INDEX'], 'filled');
+                }
+            }
+
+            // Get a list of addresses
+            let addresses = Object.keys(this.util.getAddressesList());
+
+            // Update address balances
+            await this.indexerDb.updateBalances(addresses);
+
+            // Create action mappings
+            await this.mapper.createMappings(data);
+
+        }
+    }
+}
+
+module.exports = Order_Match;
