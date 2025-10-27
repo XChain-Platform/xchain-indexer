9f2091165c4d07b1b2e94218ac38bb61275d5a13
J-Dog <j-dog@j-dog.net>
2025-10-27 15:27:48 -0700

    update tables to stop storing empty string value

--- src/sql/index_tickers.sql
+++ src/sql/index_tickers.sql
@@ -5,7 +5,3 @@ CREATE TABLE index_tickers (
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 CREATE INDEX tick on index_tickers (tick(200));
-
--- Create record for blank/empty transaction
-INSERT INTO index_tickers (id,tick) values (1,'');
-INSERT INTO index_tickers (id,tick) values (2,'GAS');
