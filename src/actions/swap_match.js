5ce5b2fda95463fbebbd40d73ca3d754d99b92f1
J-Dog <j-dog@j-dog.net>
2025-10-29 13:31:27 -0700

    cleanup `SWAP_MATCH` logic

--- src/actions/swap_match.js
+++ src/actions/swap_match.js
@@ -46,52 +46,52 @@ class Swap_Match {
         let matches = await this.indexerDb.getSwapMatches(data);
         if(matches){
             // Get information on the tokens involved in the swap
-            let getTokenInfo  = await this.indexerDb.getTokenInfo(data['GET_TICK'],  data['BLOCK_INDEX'], data['ACTION_INDEX']);
-            let giveTokenInfo = await this.indexerDb.getTokenInfo(data['GIVE_TICK'], data['BLOCK_INDEX'], data['ACTION_INDEX']);
+            let getTokenInfo  = await this.indexerDb.getTokenInfo(swapInfo['GET_TICK'],  swap['BLOCK_INDEX'], swap['ACTION_INDEX']);
+            let giveTokenInfo = await this.indexerDb.getTokenInfo(swapInfo['GIVE_TICK'], swap['BLOCK_INDEX'], swap['ACTION_INDEX']);
 
             // List of addresses allowed or blocked from holding GET_TICK
-            let getTokenAllowList = (getTokenInfo['ALLOW_LIST']) ? await this.indexerDb.getList(getTokenInfo['ALLOW_LIST']) : false;
-            let getTokenBlockList = [getTokenInfo['BLOCK_LIST']] ? await this.indexerDb.getList(getTokenInfo['BLOCK_LIST']) : false;
+            let getTokenAllowList = (!this.util.isNull(getTokenInfo['ALLOW_LIST'])) ? await this.indexerDb.getList(getTokenInfo['ALLOW_LIST']) : false;
+            let getTokenBlockList = (!this.util.isNull(getTokenInfo['BLOCK_LIST'])) ? await this.indexerDb.getList(getTokenInfo['BLOCK_LIST']) : false;
 
             // List of addresses allowed or blocked from holding GIVE_TICK
-            let giveTokenAllowList = (giveTokenInfo['ALLOW_LIST']) ? await this.indexerDb.getList(giveTokenInfo['ALLOW_LIST']) : false;
-            let giveTokenBlockList = [giveTokenInfo['BLOCK_LIST']] ? await this.indexerDb.getList(giveTokenInfo['BLOCK_LIST']) : false;
+            let giveTokenAllowList = (!this.util.isNull(giveTokenInfo['ALLOW_LIST'])) ? await this.indexerDb.getList(giveTokenInfo['ALLOW_LIST']) : false;
+            let giveTokenBlockList = (!this.util.isNull(giveTokenInfo['BLOCK_LIST'])) ? await this.indexerDb.getList(giveTokenInfo['BLOCK_LIST']) : false;
 
             // List of addresses allowed or blocked from matching with this SWAP
-            let swapInfoAllowList = (swapInfo['ALLOW_LIST']) ? await this.indexerDb.getList(swapInfo['ALLOW_LIST']) : false;
-            let swapInfoBlockList = [swapInfo['BLOCK_LIST']] ? await this.indexerDb.getList(swapInfo['BLOCK_LIST']) : false;
+            let swapInfoAllowList = (!this.util.isNull(swapInfo['ALLOW_LIST'])) ? await this.indexerDb.getList(swapInfo['ALLOW_LIST']) : false;
+            let swapInfoBlockList = (!this.util.isNull(swapInfo['BLOCK_LIST'])) ? await this.indexerDb.getList(swapInfo['BLOCK_LIST']) : false;
 
             // Loop through matches and determine if we have a valid match
-            let match = false;
-            for(let swap of matches){
+            let matchInfo = false;
+            for(let match of matches){
                 let valid = true;
 
                 // List of addresses allowed or blocked from matching with this matching SWAP
-                let matchInfoAllowList = (swap['ALLOW_LIST']) ? await this.indexerDb.getList(swap['ALLOW_LIST']) : false;
-                let matchInfoBlockList = [swap['BLOCK_LIST']] ? await this.indexerDb.getList(swap['BLOCK_LIST']) : false;
+                let matchInfoAllowList = (!this.util.isNull(match['ALLOW_LIST'])) ? await this.indexerDb.getList(match['ALLOW_LIST']) : false;
+                let matchInfoBlockList = (!this.util.isNull(match['BLOCK_LIST'])) ? await this.indexerDb.getList(match['BLOCK_LIST']) : false;
 
                 // Check if GET_ADDRESS for both sides of swap are allowed (ALLOW/BLOCK list support)
-                if((getTokenAllowList.length  && (!getTokenAllowList.includes(data['GET_ADDRESS'])  || !getTokenAllowList.includes(swap['GET_ADDRESS'])))  ||
-                   (getTokenBlockList.length  && ( getTokenBlockList.includes(data['GET_ADDRESS'])  ||  getTokenBlockList.includes(swap['GET_ADDRESS'])))  ||
-                   (giveTokenAllowList.length && (!giveTokenAllowList.includes(data['GET_ADDRESS']) || !giveTokenAllowList.includes(swap['GET_ADDRESS']))) ||
-                   (giveTokenBlockList.length && ( giveTokenBlockList.includes(data['GET_ADDRESS']) ||  giveTokenBlockList.includes(swap['GET_ADDRESS']))) ||
-                   (swapInfoAllowList.length  && !swapInfoAllowList.includes(swap['GET_ADDRESS']))  ||
-                   (swapInfoBlockList.length  &&  swapInfoBlockList.includes(swap['GET_ADDRESS']))  || 
-                   (matchInfoAllowList.length && !matchInfoAllowList.includes(data['GET_ADDRESS'])) ||
-                   (matchInfoBlockList.length &&  matchInfoBlockList.includes(data['GET_ADDRESS']))){
+                if((getTokenAllowList.length  && (!getTokenAllowList.includes(swapInfo['GET_ADDRESS'])  || !getTokenAllowList.includes(match['GET_ADDRESS'])))  ||
+                   (getTokenBlockList.length  && ( getTokenBlockList.includes(swapInfo['GET_ADDRESS'])  ||  getTokenBlockList.includes(match['GET_ADDRESS'])))  ||
+                   (giveTokenAllowList.length && (!giveTokenAllowList.includes(swapInfo['GET_ADDRESS']) || !giveTokenAllowList.includes(match['GET_ADDRESS']))) ||
+                   (giveTokenBlockList.length && ( giveTokenBlockList.includes(swapInfo['GET_ADDRESS']) ||  giveTokenBlockList.includes(match['GET_ADDRESS']))) ||
+                   (swapInfoAllowList.length  && !swapInfoAllowList.includes(match['GET_ADDRESS']))     ||
+                   (swapInfoBlockList.length  &&  swapInfoBlockList.includes(match['GET_ADDRESS']))     || 
+                   (matchInfoAllowList.length && !matchInfoAllowList.includes(swapInfo['GET_ADDRESS'])) ||
+                   (matchInfoBlockList.length &&  matchInfoBlockList.includes(swapInfo['GET_ADDRESS']))){
                     valid = false;
                 }
 
                 // If we found a valid match, stop looking for additional matches
                 if(valid){
-                    match = swap;
+                    matchInfo = match;
                     break;
                 }
             }
 
             // Process the swap match
             // TODO : Revisit this code once multi-chain swap support is added to xchain-hub component
-            if(match){
+            if(matchInfo){
 
                 // Set the status to valid
                 data['STATUS'] = 'valid';
@@ -113,30 +113,27 @@ class Swap_Match {
                 // Create a record of this SWAP_MATCH action in the actions table
                 data['ACTION_INDEX'] = await this.indexerDb.createActionIndex(action);
 
-                // Store the SOURCE and GET_TICK in addresses list
-                this.util.addAddressTicker(match['SOURCE'], match['GET_TICK']);
-
                 // Credit tokens to GET_ADDRESS in swaps
-                credits.push([match['GET_TICK'], match['GET_AMOUNT'], match['GET_ADDRESS']]);
-                credits.push([swap['GET_TICK'],  swap['GET_AMOUNT'],  swap['GET_ADDRESS']]);
+                credits.push([matchInfo['GET_TICK'], matchInfo['GET_AMOUNT'], matchInfo['GET_ADDRESS']]);
+                credits.push([swapInfo['GET_TICK'],  swapInfo['GET_AMOUNT'],  swapInfo['GET_ADDRESS']]);
 
                 // Debit tokens from escrows table 
-                escrows.push([match['GET_TICK'], -match['GET_AMOUNT'], match['SOURCE']]);
-                escrows.push([swap['GET_TICK'],  -swap['GET_AMOUNT'],  swap['SOURCE']]);
+                escrows.push([matchInfo['GET_TICK'], -matchInfo['GET_AMOUNT'], swapInfo['SOURCE']]);
+                escrows.push([swapInfo['GET_TICK'],  -swapInfo['GET_AMOUNT'],  matchInfo['SOURCE']]);
 
-                // Store the GET_ADDRESS and TICK in addresses list
-                this.util.addAddressTicker(match['GET_ADDRESS'], match['GET_TICK']);
-                this.util.addAddressTicker(swap['GET_ADDRESS'],  swap['GET_TICK']);
+                // Store the GET_ADDRESS and GET_TICK in addresses list
+                this.util.addAddressTicker(matchInfo['GET_ADDRESS'], matchInfo['GET_TICK']);
+                this.util.addAddressTicker(swapInfo['GET_ADDRESS'],  swapInfo['GET_TICK']);
 
                 // Process any transaction ledger changes (credits / debits / escrows)
                 await this.util.processTransactionLedgerChanges(this.indexerDb, data, credits, debits, escrows);
 
                 // Create record of match in swap_matches table
-                await this.indexerDb.createSwapMatch(data, swap, match);
+                await this.indexerDb.createSwapMatch(data, swapInfo, matchInfo);
 
                 // Update record in swaps table to change status (open->complete)
-                await this.indexerDb.createSwapStatus(data['ACTION_INDEX'], swap['ACTION_INDEX'],  'complete');
-                await this.indexerDb.createSwapStatus(data['ACTION_INDEX'], match['ACTION_INDEX'], 'complete');
+                await this.indexerDb.createSwapStatus(data['ACTION_INDEX'], swapInfo['ACTION_INDEX'],  'complete');
+                await this.indexerDb.createSwapStatus(data['ACTION_INDEX'], matchInfo['ACTION_INDEX'], 'complete');
 
                 // Get a list of addresses
                 let addresses = Object.keys(this.util.getAddressesList());
