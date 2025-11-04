125f90923f7aa34ae6dbf8008ecdfe6658617ddd
J-Dog <j-dog@j-dog.net>
2025-11-04 13:28:27 -0800

    store block_index in actions table updates
    - add block_index to actions table since in cases of MATCH and EXPIRE actions, there is no tx_index to reference, so need to store block_index to know when action took place
    - update getActionIndex() and createActionIndex() to handle NULL values properly
    - updated createActionIndex() to store block_index field

--- src/sql/actions.sql
+++ src/sql/actions.sql
@@ -3,12 +3,14 @@
 DROP TABLE IF EXISTS actions;
 CREATE TABLE actions (
   action_index  BIGINT UNSIGNED NOT NULL, -- Unique index for every action
-  tx_index      BIGINT UNSIGNED NOT NULL, -- tx_index from the transactions table
+  block_index   BIGINT UNSIGNED NOT NULL, -- block_index from the blocks table
+  tx_index      BIGINT UNSIGNED,          -- tx_index from the transactions table
   action_id     BIGINT UNSIGNED NOT NULL, -- id of record in index_actions table
   action_format TINYINT UNSIGNED          -- FORMAT of action data (0-255)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 CREATE UNIQUE INDEX action_index    on actions (action_index);
+CREATE        INDEX block_index     on actions (block_index);
 CREATE        INDEX tx_index        on actions (tx_index);
 CREATE        INDEX action_id       on actions (action_id);
 CREATE        INDEX action_format   on actions (action_format);
