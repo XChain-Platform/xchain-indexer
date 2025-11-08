5777a01aee59a2cc57b3b11cf1e786d5d667fdd6
J-Dog <j-dog@j-dog.net>
2025-11-07 23:42:45 -0800

    add support for UNKNOWN command

---
+++ src/actions/unknown.js
@@ -0,0 +1,54 @@
+/*********************************************************************
+ * 
+ * Copyright © 2025 Dankest, LLC
+ * Based on XChain Platform by Dankest, LLC – https://dankest.llc
+ *
+ * Licensed under the Dankest Community License (Apache License 2.0 + Additional Terms).
+ * You may not use this file except in compliance with that License.
+ * 
+ * A copy of the License is available at:
+ *     https://dankest.llc/license
+ *
+ * This software is provided “AS IS”, without warranties or conditions of any kind.
+ * 
+ **********************************************************************
+ *
+ * XChain Platform Action - UNKNOWN
+ * 
+ * This is a generalized action class for any unknown ACTION commands
+ * 
+ ********************************************************************/
+
+class Unknown {
+
+    // Handle constructing a class instance
+    constructor(action){
+        // Setup short aliases
+        this.actions   = action;
+        this.config    = action.config;
+        this.decoderDb = action.decoderDb;
+        this.indexerDb = action.indexerDb;
+        this.util      = action.util;
+        this.mapper    = action.mapper;
+    }
+
+    // Handle parsing the UNKNOWN transactions
+    async parse(params, data, error){
+
+        // Determine final status
+        let status = (error) ? error : 'invvalid';
+        data['STATUS'] = status;
+
+        // Print status message 
+        console.log("\t UNKNOWN : " + data['STATUS']);
+
+        // Store the SOURCE in addresses list
+        this.util.addAddressTicker(data['SOURCE']);
+
+        // Create action mappings
+        await this.mapper.createMappings(data);
+
+    }
+}
+
+module.exports = Unknown;
